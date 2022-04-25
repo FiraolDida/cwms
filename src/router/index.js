52a7b44fd73ee8e5dd6615ffd6df72074ae5704c
firaoldida <firaoldida1@gmail.com>
2022-04-25 10:12:55 +0300

    Added default layout as parent route

--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,32 @@ import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import User from '../views/User.vue';
 import Organization from '../views/Organization.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import store from "../store";
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
-    },
-    {
-        path: '/user',
-        name: 'User',
-        component: User
-    },
-    {
-        path: '/organization',
-        name: 'Organization',
-        component: Organization
+        redirect: '/dashboard',
+        component: DefaultLayout,
+        meta: { requiresAuth: true },
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/user',
+                name: 'User',
+                component: User
+            },
+            {
+                path: '/organization',
+                name: 'Organization',
+                component: Organization
+            },
+        ]
     },
     {
         path: '/login',
@@ -30,6 +40,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'Login' })
+    } else if (store.state.user.token && (to.meta.isGuest)) {
+        next({ name: 'Dashboard'});
+    } else {
+        next()
+    }
 })
 
 export default router;
